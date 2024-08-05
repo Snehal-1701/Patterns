34a5984701b0ae650e7e85b15884c85e03c79c7e
snehalkangude1701 <snehalkangude1701@gmail.com>
2024-08-05 21:02:03 +0530

    Pattern code

---
+++ code2.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Demo1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. of rows : ");
+        int row = sc.nextInt();
+        int column = row + (row-1);
+        int mid = column/2;
+        for(int i=1; i<=row; i++) {
+            for(int j=0; j<mid; j++) {
+                System.out.print("  ");
+            }
+           
+            for(int j=1; j<=column-(2*mid); j++) {
+                if(j%2 == 1) {
+                    System.out.print("* ");
+                }else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+            mid--;
+        }
+    }
+}
